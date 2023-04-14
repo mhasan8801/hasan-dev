bced6ead1e49857b8d90f397dd3fce733c0725cb
M Hasan <hasanmuhammad197@gmail.com>
2023-04-15 06:58:31 +0700

    initial project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'typeface-roboto'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Hasan</h1>
+      <button type="button" class="btn btn-primary">Primary</button>
+    </div>
+  )
+}
+
+export default App

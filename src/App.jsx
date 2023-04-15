92bb929d30fafb743ff71a3beb32d78fe3d3a907
M Hasan <hasanmuhammad197@gmail.com>
2023-04-16 05:00:08 +0700

    add: hero

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,15 @@ import Title from './elements/Title'
 import Paragraph from './elements/Paragraph'
 import Navbar from './components/Navbar'
 import logo from './assets/images/logo.png'
+import Hero from './components/Hero/Hero'
+import './App.css'
 
 function App() {
 
   return (
       <div>
         <Navbar/>
+        <Hero/>
       </div>
   )
 }

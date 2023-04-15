6ef3ec49c918c305c7d14745cbc42d595b64a37b
M Hasan <hasanmuhammad197@gmail.com>
2023-04-15 07:15:49 +0700

    add: element color, button, title, paragraph

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'typeface-roboto'
+import Button from './elements/Button'
+import Title from './elements/Title'
+import Paragraph from './elements/Paragraph'
 
 function App() {
 
   return (
     <div className="App">
-      <h1>Hasan</h1>
-      <h2>Initial Develop</h2>
-      <h2>Initial Feature</h2>
-      <button type="button" class="btn btn-primary">Primary</button>
+      <div className="title">
+        <Title>Hasan</Title>
+        <Title $h2>Hasan</Title>
+        <Title $h3>Hasan</Title>
+      </div>
+      <div className="paragraph">
+        <Paragraph>Lorem ipsum dolor sit amet consectetur adipisicing elit. </Paragraph>
+        <Paragraph $p2>Lorem ipsum dolor sit amet consectetur adipisicing elit. </Paragraph>
+        <Paragraph $p3>Lorem ipsum dolor sit amet consectetur adipisicing elit. </Paragraph>
+      </div>
+      <div className="button">
+        <Button>Normal</Button>
+        <Button $primary>Primary Button</Button>
+      </div>
     </div>
   )
 }

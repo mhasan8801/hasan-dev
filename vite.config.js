052465266c4522ea4e7cd407219fa7c83a15b6f5
M Hasan <hasanmuhammad197@gmail.com>
2023-05-09 12:52:19 +0700

    add: rollup

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  build: {
+    rollupOptions: {
+      input: 'src/main.js'
+    }
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+      '@assets': '/src/assets'
+    }
+  }
+
 })
 
